54e91e45b05ac757f14dbf0eb68982e7c66bf39d
Alkihis <alkihis@gmail.com>
2020-07-26 23:22:14 +0200

    Interests

--- components/Interests.vue
+++ components/Interests.vue
@@ -4,14 +4,54 @@
       Centres d'intérêt
     </h1>
 
-    <p class="text">
-      Des trucs
-    </p>
+    <div class="text">
+      <h2>
+        Informatique
+      </h2>
+      <p>
+        Maintenance et administration de machines personnelles sous différents systèmes d'exploitations, 
+        développement et participation dans la communauté JavaScript.
+      </p>
+
+      <h2>
+        Transports
+      </h2>
+      <p>
+        Transports collectifs ou individuels, modes doux, multi-modalité, et matériel roulant.
+      </p>
+
+      <h2>
+        Urbanisme
+      </h2>
+      <p>
+        Organisation et aménagement de l'espace urbain.
+      </p>
+
+      <h2>
+        Loisirs
+      </h2>
+      <p>
+        Randonnée, vélo, jeux vidéo, et veille technologique permanente.
+      </p>
+    </div>
   </main>
 </template>
 
 <style lang="scss" scoped>
+  h2 {
+    color: rgb(58, 100, 179);
+    font-size: 1.5rem;
+    margin-bottom: .7rem;
+    margin-top: 1.5rem;
 
+    &:first-child {
+      margin-top: 0;
+    }
+  }
+
+  h2 + p {
+    margin: 0;
+  }
 </style>
 
 <script lang="ts">
@@ -20,7 +60,5 @@ import { Vue, Component } from 'nuxt-property-decorator';
 @Component({
   components: {}
 })
-export default class extends Vue {
-
-}
+export default class extends Vue { }
 </script>
